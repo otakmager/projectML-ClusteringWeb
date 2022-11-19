1cd7b4d25b97b9daee1eebb6edbc950650eaca31
Dimas Wahyu Ardiyanto <dimazwahyuardiyanto@gmail.com>
2022-11-19 16:39:11 +0700

    Finishing and give validation check

--- app.py
+++ app.py
@@ -1,37 +1,38 @@
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, render_template, request
 import numpy as np
 import pandas as pd
 import pickle
-from pyexpat import features
-from flask_cors import CORS, cross_origin
 
+# Inisialisasi aplikasi and model prediction
 app = Flask(__name__)
 model = pickle.load(open("best_model.pkl", "rb"))
-CORS(app, support_credentials=True)
 
-@app.route("/", methods=["GET", "POST"])
+# Routing aplikasi awal -> menampilkan index page
+@app.route("/")
 def home():
-    if request.method == "POST":
-        
-        int_features = np.array([int(x) for x in request.form.values()])
-        features = np.array([int_features])
-        prediction = model.predict(features)
-        return f'{prediction}'
     return render_template('index.html')
 
+# Routing /prediksi -> prediksi class object
 @app.route("/predict", methods=["POST"])
-@cross_origin(supports_credentials=True)
-@cross_origin(origin='localhost',headers=['Content- Type','Authorization'])
 def predict():
-    if request.method == 'POST':
-        return render_template('index.html', request.form.get("lymphatic"))
-    
-    # # int_features = [int(x) for x in request.form.values()]
-    # int_features = request.form.values()
-    # features = [np.array[int_features]]
-    # prediction = model.predict(features)
-    # return render_template("index.html", prediction_text = "OK")
-    # # return render_template("index.html", prediction_text = "{}".format(prediction))
+    # Simpan data fitur object dalam array dan diubah ke integer 
+    int_features = np.array([int(x) for x in request.form.values()])
+    # Buat dalam array 2D karena model hanya menerima 2D array
+    features = np.array([int_features])
+    # Prediksi class object dengan model yang telah dibuat
+    prediction = model.predict(features)
+    # Karena class prediksi berupa int, ubah ke bentuk tulisan
+    if(prediction==[1]):
+        res = "Normal Find"
+    elif(prediction==[2]):
+        res = "Metastases"
+    elif(prediction==[3]):
+        res = "Malign lymph"
+    elif(prediction==[4]):
+        res = "Fibrosis"
+    # Mengembalikan hasil prediksi ke index.html dalam variabel result
+    return render_template("index.html", result = "{}".format(res))
 
+# Menjalankan aplikasi dengan konfigurasi host dan debug mode
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True)
